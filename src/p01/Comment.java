b03e0197904750d20034e1366389e0e8e45515d1
tpdn333 <tpdn333@gmail.com>
2021-03-25 10:28:22 +0900

    first commit !

---
+++ src/p01/Comment.java
@@ -0,0 +1,15 @@
+package p01;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.println("주석");
+		
+		// 한줄 주석은 slash 2개
+		System.out.println("한 줄 주석");
+		
+		/*
+		 * 여러줄 주석은 slash* ~ *slash
+		 */
+		System.out.println("여러 줄 주석");
+	}
+}
