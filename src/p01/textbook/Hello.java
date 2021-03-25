243a8be9dc5ef73574693ce19e663405fcb256af
tpdn333 <tpdn333@gmail.com>
2021-03-25 11:33:31 +0900

    Chapter 01

---
+++ src/p01/textbook/Hello.java
@@ -0,0 +1,13 @@
+package p01.textbook;
+
+/*
+ * 작성자 : 신용권
+ * 작성일 : 2021.30.25
+ */
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, welcom to the java world!");
+	} // end of main
+} // end of class
