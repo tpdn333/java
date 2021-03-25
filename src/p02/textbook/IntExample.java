bd2706d25dbf2e46833adca2e977e72ef2d39658
tpdn333 <tpdn333@gmail.com>
2021-03-25 16:32:07 +0900

    Type

---
+++ src/p02/textbook/IntExample.java
@@ -0,0 +1,14 @@
+package p02.textbook;
+
+public class IntExample {
+	public static void main(String[] args) {
+		int var1 = 10;  // 10진법
+		int var2 = 012; // 8진법
+		int var3 = 0xa; // 16진법
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+	}
+}
