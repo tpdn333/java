3befc46054d99c780b197dba139b348b9d9d2c73
tpdn333 <tpdn333@gmail.com>
2021-03-26 12:27:34 +0900

    Type

--- src/p02/textbook/FromIntToFloat.java
+++ src/p02/textbook/FromIntToFloat.java
@@ -5,7 +5,7 @@ public class FromIntToFloat {
 		int num1 = 123456780;
 		int num2 = 123456780;
 
-		float num3 = num2;
+		float num3 = num2;  // 자동 타입 변환
 		num2 = (int) num3;
 
 		int result = num1 - num2;
