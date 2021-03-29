17e071ec934b5e0ef243a7f508da9dde2cba4632
tpdn333 <tpdn333@gmail.com>
2021-03-29 10:19:17 +0900

    unary

---
+++ src/p03/lecture/p1unary/A04BitNotMethod.java
@@ -0,0 +1,31 @@
+package p03.lecture.p1unary;
+
+public class A04BitNotMethod {
+	public static void main(String[] args) {
+		// 75p code 설명
+
+		// JS :
+		// function (funcName(param1, param2) {
+		// statement.......;
+		// }
+
+		// 함수호출
+		// funcName(1,2);
+
+		/////////////////////////////////////////////
+		// java 함수호출
+		funcName(5, 6);
+	}
+
+	public static int funcName(int para1, int para2) {
+		// statement;
+
+		return 3;
+	}
+
+	public static String funcName1(int para1, int para2) {
+		// statement;
+
+		return "hello";
+	}
+}
