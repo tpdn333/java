8468acbdfb19efebf8c023993098da5b73d20723
tpdn333 <tpdn333@gmail.com>
2021-03-31 16:35:39 +0900

    두 정수 사이의 합

---
+++ src/p99/programmers/p12912.java
@@ -0,0 +1,20 @@
+package p99.programmers;
+
+public class p12912 {
+	public long solution(int a, int b) {
+		long answer = 0;
+		if (a == b) {
+			return a;
+		} else if (a > b) {
+			for (int i = b; i <= a; i++) {
+				answer += i;
+			}
+		} else {
+			for (int i = a; i <= b; i++) {
+				answer += i;
+			}
+		}
+		return answer;
+	}
+	
+}
