c0b07e0aacb8bf864ae496a3e5de97724ea7c246
tpdn333 <tpdn333@gmail.com>
2021-03-31 17:52:18 +0900

    programmers

---
+++ src/p99/programmers/p12906.java
@@ -0,0 +1,15 @@
+package p99.programmers;
+
+//import java.util.*;
+
+public class p12906 {
+	public static void main(String[] args) {
+		
+		int[] arr = {1,1,3,3,0,1,1};
+		int[] answer = {};
+		int cnt = 1;
+//		answer[cnt] = 1;
+		arr[0] = 2;
+		System.out.println(arr[0]);
+	}
+}
