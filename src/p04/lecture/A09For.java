b5320d6e6fffaaca16ed21ec201419a46a7143d0
tpdn333 <tpdn333@gmail.com>
2021-03-31 10:27:12 +0900

    For

---
+++ src/p04/lecture/A09For.java
@@ -0,0 +1,13 @@
+package p04.lecture;
+
+public class A09For {
+	public static void main(String[] args) {
+		
+		System.out.println("실행문1");
+		
+		for (int i = 0; i < 3; i++){
+			System.out.println("반복할 코드들...");
+		}
+		System.out.println("실행문");
+	}
+}
