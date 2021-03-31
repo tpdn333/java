800aec00f2ac40808434495390bf95ba6bb02062
tpdn333 <tpdn333@gmail.com>
2021-03-31 10:40:04 +0900

    1To10

---
+++ src/p04/textbook/ForPrintFrom1To10Example.java
@@ -0,0 +1,29 @@
+package p04.textbook;
+
+public class ForPrintFrom1To10Example {
+	public static void main(String[] args) {
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println(i);
+//		}
+		// 1~10 홀수만
+//		for (int i = 1; i <= 10; i += 2) {
+//			System.out.println(i);
+//		}
+		// 1~10 짝수만
+//		for (int i = 2; i <= 10; i += 2) {
+//			System.out.println(i);
+//		}
+		// 10~1
+//		for (int i = 10; i > 0; i--) {
+//			System.out.println(i);
+//		}
+		// 10~1 짝수만
+//		for (int i = 10; i >= 0; i -= 2) {
+//			System.out.println(i);
+//		}
+		// 10~1 홀수만
+		for (int i = 9; i >= 0; i -= 2) {
+			System.out.println(i);
+		}
+	}
+}
