baf6b6bb6d86f909059f047a90c150af25148118
tpdn333 <tpdn333@gmail.com>
2021-04-02 15:18:17 +0900

    array

---
+++ src/p05/lecture/A18ArraySample.java
@@ -0,0 +1,24 @@
+package p05.lecture;
+
+public class A18ArraySample {
+	public static void main(String[] args) {
+		int[] arr1 = new int[] { 3, 4, 5, 6, 7 };
+		int[] arr2 = new int[] { 99, 88, 70 };
+
+		double avg1 = avg(arr1);
+		double avg2 = avg(arr2);
+		System.out.println(avg1);
+		System.out.println(avg2);
+
+	}
+
+	public static double avg(int[] arr) {
+		int sum = 0;
+		double avg = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		return sum / (double) arr.length;
+		
+	}
+}
---
+++ src/p05/lecture/A19ArrayInitValue.java
@@ -0,0 +1,16 @@
+package p05.lecture;
+
+public class A19ArrayInitValue {
+	public static void main(String[] args) {
+		int[] arr1 = new int[3];
+		int[] arr2 = new int[] {55, 66, 77};
+		int[] arr3 = {3, 4, 5, 6, 7};  // new int[] 생략가능
+		
+		int[] arr4;
+		arr4 = new int[] {9, 8, 7};
+		
+		int[] arr5;
+//		arr55 = {34, 56, 78};  // 컴파일 오류 선언 당시에만 생략가능
+		
+	}
+}
