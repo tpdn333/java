8965bfb067d7237d1bb41966685864b74f2172a6
tpdn333 <tpdn333@gmail.com>
2021-04-02 14:10:19 +0900

    arrayField

---
+++ src/p05/lecture/A10Array.java
@@ -0,0 +1,15 @@
+package p05.lecture;
+
+public class A10Array {
+	public static void main(String[] args) {
+		// array (배열) : 여러개의 값을 갖는 자료형
+		//				  같은 타입으로 채워야됨
+	
+		int i;
+		i = 5;
+		
+		int[] arr; // 배열도 참조타입
+		arr = new int[3];
+		arr[0] = 2;
+	}
+}
