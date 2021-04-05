96fb9afbd82bbfad19b02f504abea48e1c28ffd3
tpdn333 <tpdn333@gmail.com>
2021-04-05 11:44:00 +0900

    forEach

---
+++ src/p05/lecture/A23ForEach.java
@@ -0,0 +1,17 @@
+package p05.lecture;
+
+public class A23ForEach {
+	public static void main(String[] args) {
+		int[] arr1 = { 9, 8, 7, 6 };
+
+		// for
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.println(arr1[i]);
+		}
+
+		// for each
+		for (int n : arr1) {
+			System.out.println(n);
+		}
+	}
+}
