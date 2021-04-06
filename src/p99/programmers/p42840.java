ec8ca02599b8de98c75386dd0dfc493b61c2e0da
tpdn333 <tpdn333@gmail.com>
2021-04-06 15:51:15 +0900

    수포자

---
+++ src/p99/programmers/p42840.java
@@ -0,0 +1,18 @@
+package p99.programmers;
+
+public class p42840 {
+	public static void main(String[] args) {
+		int[] user1 = { 1, 2, 3, 4, 5 };
+		int[] user2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+		int[] user3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+		int[] answer = { 1, 3, 2, 4, 2 };
+		
+		for(int i = 0; i < answer.length; i++) {
+			if(answer[i] == user1[i]) {
+				
+			}
+		}
+			
+		
+	}
+}
