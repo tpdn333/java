7c9cf2de32223417a784f7569bf85c5cabeaa130
tpdn333 <tpdn333@gmail.com>
2021-04-06 14:25:13 +0900

    class

---
+++ src/p06/lecture/p01class/Student.java
@@ -0,0 +1,12 @@
+package p06.lecture.p01class;
+// 객체를 만들기 위한 설계도
+
+// class명 : UpperCamleCase
+// 숫자, 영문, $, _ 조합: 숫자가 앞에 오면 안됨
+
+// class 키워드가 앞에 옴
+// class명 다음에 class 본문을 중괄호{}안에 작성
+
+public class Student {
+
+}
