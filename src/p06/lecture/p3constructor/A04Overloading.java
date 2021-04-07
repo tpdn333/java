2b3d0674dd95bc09b03f1fa8ff6ebfb34417cd34
tpdn333 <tpdn333@gmail.com>
2021-04-07 14:11:16 +0900

    constructor

--- src/p06/lecture/p3constructor/A04Overloading.java
+++ src/p06/lecture/p3constructor/A04Overloading.java
@@ -1,6 +1,6 @@
 package p06.lecture.p3constructor;
 
-public class A03Overloading {
+public class A04Overloading {
 	public static void main(String[] args) {
 		Computer c1 = new Computer("dell", 4, "intel", 128);
 		Computer c2 = new Computer("hp", 8, "amd", 256);
