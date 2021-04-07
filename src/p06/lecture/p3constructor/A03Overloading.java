479bce33a410664cb75803d3cfe418ab1e72923c
tpdn333 <tpdn333@gmail.com>
2021-04-07 11:01:58 +0900

    this

---
+++ src/p06/lecture/p3constructor/A03Overloading.java
@@ -0,0 +1,8 @@
+package p06.lecture.p3constructor;
+
+public class A03Overloading {
+	public static void main(String[] args) {
+		Computer c1 = new Computer("dell", 4, "intel", 128);
+		Computer c2 = new Computer("hp", 8, "amd", 256);
+	}
+}
---
+++ src/p06/lecture/p3constructor/A05ThisConstructor.java
@@ -0,0 +1,10 @@
+package p06.lecture.p3constructor;
+
+public class A05ThisConstructor {
+	// this : 이 클래스에 의해 만들어지는 참조변수를 대체 키워드
+	// this() : 이 클래스 내의 다른 생성자
+	public static void main(String[] args) {
+		Notebook n1 = new Notebook("lenovo", 32, "intel");
+		
+	}
+}
