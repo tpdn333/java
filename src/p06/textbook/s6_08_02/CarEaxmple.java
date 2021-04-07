94e924e76dd94b080b87884f9fb7af654c819723
tpdn333 <tpdn333@gmail.com>
2021-04-07 14:45:38 +0900

    textbook

---
+++ src/p06/textbook/s6_08_02/CarEaxmple.java
@@ -0,0 +1,22 @@
+package p06.textbook.s6_08_02;
+
+public class CarEaxmple {
+	public static void main(String[] args) {
+		Car myCar = new Car();
+
+		myCar.setGas(5);
+
+		boolean gasState = myCar.isLeftGas();
+		if (gasState) {
+			System.out.println("출발합니다.");
+			myCar.run();
+		}
+
+		if (myCar.isLeftGas()) {
+			System.out.println("gas를 주입할 필요가 있습니다.");
+		} else {
+			System.out.println("gas를 주입하세요.");
+		}
+
+	}
+}
