49f2da78fd6217055355e6787e9d12fd53b82bc3
tpdn333 <tpdn333@gmail.com>
2021-04-08 12:26:14 +0900

    instanceMember

---
+++ src/p06/lecture/p5static/A01InstanceMember.java
@@ -0,0 +1,10 @@
+package p06.lecture.p5static;
+
+public class A01InstanceMember {
+	public static void main(String[] args) {
+		MyClass1 o1 = new MyClass1();
+		o1.i = 3;
+		o1.method();
+		
+	}
+}
---
+++ src/p06/lecture/p5static/MyClass1.java
@@ -0,0 +1,10 @@
+package p06.lecture.p5static;
+
+public class MyClass1 {
+	int i;
+	
+	void method() {
+		
+	}
+	
+}
