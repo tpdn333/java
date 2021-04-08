f0b228117c58ba091b9a5fe5b8de3b591c6f353b
tpdn333 <tpdn333@gmail.com>
2021-04-08 12:25:50 +0900

    textbook

---
+++ src/p06/textbook/exercise/ex17/Printer.java
@@ -0,0 +1,19 @@
+package p06.textbook.exercise.ex17;
+
+public class Printer {
+	static void printer(int a) {
+		System.out.println(a);
+	}
+
+	static void printer(boolean a) {
+		System.out.println(a);
+	}
+
+	static void printer(double a) {
+		System.out.println(a);
+	}
+
+	static void printer(String a) {
+		System.out.println(a);
+	}
+}
---
+++ src/p06/textbook/exercise/ex17/PrinterExample.java
@@ -0,0 +1,11 @@
+package p06.textbook.exercise.ex17;
+
+public class PrinterExample {
+	public static void main(String[] args) {
+		Printer.printer(10);
+		Printer.printer(true);
+		Printer.printer(5.7);
+		Printer.printer("홍길동");
+	}
+
+}
