c8c2c5d16d1b7f4b5de564b5eb4c0ec02c31ecff
tpdn333 <tpdn333@gmail.com>
2021-04-08 14:34:31 +0900

    staticBlock

---
+++ src/p06/textbook/s6_10_03/Television.java
@@ -0,0 +1,12 @@
+package p06.textbook.s6_10_03;
+
+public class Television {
+	static String company = "Samsung";
+	static String model = "LCD";
+	static String info;
+
+	static {
+		info = company + "-" + model;
+	}
+
+}
---
+++ src/p06/textbook/s6_10_03/TelevisionExample.java
@@ -0,0 +1,7 @@
+package p06.textbook.s6_10_03;
+
+public class TelevisionExample {
+	public static void main(String[] args) {
+		System.out.println(Television.info);
+	}
+}
