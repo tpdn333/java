4eb5ec9bbfce7198647a997d9732c0b9b6d76a67
tpdn333 <tpdn333@gmail.com>
2021-04-08 12:26:21 +0900

    staticMember

---
+++ src/p06/lecture/p5static/MyClass2.java
@@ -0,0 +1,22 @@
+package p06.lecture.p5static;
+
+public class MyClass2 {
+	// instance member
+	// instance field
+	int j;
+	
+	// instance method
+	void method1() {
+		System.out.println("method not static");
+	}
+	
+	// class(static) member
+	// class field ( static field )
+	static int i;
+	
+	// class method ( static method )
+	static void method() {
+		System.out.println("method static");
+	}
+	
+}
