7374c16df292ea77d0992b420abedca660270aba
tpdn333 <tpdn333@gmail.com>
2021-04-09 11:49:25 +0900

    package

---
+++ src/p06/lecture/p7package/A02Package.java
@@ -0,0 +1,10 @@
+package p06.lecture.p7package;
+
+public class A02Package {
+	public static void main(String[] args) {
+		p06.lecture.p7package.package1.MyClass1 o1 = new p06.lecture.p7package.package1.MyClass1();
+		
+		MyClass3 o3 = new MyClass3();
+		
+	}
+}
