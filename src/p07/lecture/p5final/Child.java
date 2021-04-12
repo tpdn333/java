2d77609c2a45ae7458898118884cffdc80840b50
tpdn333 <tpdn333@gmail.com>
2021-04-12 14:19:29 +0900

    final

--- src/p07/lecture/p5final/Child.java
+++ src/p07/lecture/p5final/Child.java
@@ -1,5 +1,14 @@
 package p07.lecture.p5final;
 
 public final class Child extends Parent{
-
+	@Override 
+	public void method1() {
+		System.out.println("method override");
+	}
+	
+	// final 메소드는 오버라이딩 X
+//	@Override
+//	public void method2() {
+//		System.out.println("method override");
+//	}
 }
