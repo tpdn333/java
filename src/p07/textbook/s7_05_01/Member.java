19328990b47c1267b14058e0dbb2008f9af9e964
tpdn333 <tpdn333@gmail.com>
2021-04-12 14:09:27 +0900

    textbook

---
+++ src/p07/textbook/s7_05_01/Member.java
@@ -0,0 +1,5 @@
+package p07.textbook.s7_05_01;
+
+public final class Member {
+
+}
---
+++ src/p07/textbook/s7_05_01/VeryVeryImportantPerson.java
@@ -0,0 +1,8 @@
+package p07.textbook.s7_05_01;
+// Member를 상속할 수 없음
+//public class VeryVeryImportantPerson extends Member{
+//
+//}
+public class VeryVeryImportantPerson{
+	
+}
