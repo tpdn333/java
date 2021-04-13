d057734d2088071f3df185bdfb36affc4c5e4c2c
tpdn333 <tpdn333@gmail.com>
2021-04-13 14:18:31 +0900

    abstract

---
+++ src/p07/lecture/p6polymorphism/ex3/A01AbstractCalss.java
@@ -0,0 +1,10 @@
+package p07.lecture.p6polymorphism.ex3;
+
+public class A01AbstractCalss {
+	public static void main(String[] args) {
+		KindaCat k1 = new Cat();
+		KindaCat k2 = new Tiger();
+		
+//		KindaCat k3 = new KindaCat();
+	}
+}
---
+++ src/p07/lecture/p6polymorphism/ex3/KindaCat.java
@@ -0,0 +1,9 @@
+package p07.lecture.p6polymorphism.ex3;
+
+public abstract class KindaCat {
+	
+	// 하는일이 명확하지 않을 때 추상적으로 설정해주는 키워드
+	// 설정해주면 sub Class에 꼭 추가해줘야됨
+	// 추가해주지 않을꺼면 sub class에서 abstract 추가!
+	public abstract void cry();  
+}
