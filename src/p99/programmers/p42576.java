bd37b59f1174d407583a2ffbb96f2273e632ea3b
tpdn333 <tpdn333@gmail.com>
2021-04-14 15:52:25 +0900

    programmers

---
+++ src/p99/programmers/p42576.java
@@ -0,0 +1,14 @@
+package p99.programmers;
+
+import java.util.Arrays;
+
+public class p42576 {
+	public static void main(String[] args) {
+		String[] participant = { "leo", "kiki", "eden" };
+		String[] completion = {"eden", "kiki"};
+		String answer = "";
+		Arrays.sort(participant);
+		System.out.println(Arrays.toString(participant));
+	}
+	
+}
