08ac006d273ac93e7f6ac75aff6f5a27c3f73dd4
tpdn333 <tpdn333@gmail.com>
2021-04-14 15:52:19 +0900

    codingbat

--- src/p99/codingbat/Warmup_2/stringMatch.java
+++ src/p99/codingbat/Warmup_2/stringMatch.java
@@ -1,18 +1,42 @@
 package p99.codingbat.Warmup_2;
 
 public class stringMatch {
-	public static void main(String[] args) {
-		String a = "xxcaazz";
-		String b = "xxbaaz";
-		int result = 0;
-		for (int i = 0; i < a.length() - 1; i++) {
-			for (int j = 0; i < b.length()-1; j++) {
-				if(a.substring(i, i+1).equals(b.substring(j,j+1))) {
-					result++;
-				}
+//	public static void main(String[] args) {
+//		String a = "xxcaazz";
+//		String b = "xxbaaz";
+//		int result = 0;
+//		for (int i = 0; i < a.length() - 1; i++) {
+//			for (int j = 0; j < b.length() - 1; j++) {
+//				if (a.substring(i, i + 2).equals(b.substring(j, j + 2))) {
+//					result++;
+//				}
+//			}
+//		}
+//		System.out.println(result);
+//	}
+
+	public static int stringMatch(String a, String b) {
+		// Figure which string is shorter.
+		int len = Math.min(a.length(), b.length());
+		int count = 0;
+
+		// Look at both substrings starting at i
+		for (int i = 0; i < len - 1; i++) {
+			String aSub = a.substring(i, i + 2);
+			String bSub = b.substring(i, i + 2);
+			if (aSub.equals(bSub)) { // Use .equals() with strings
+				count++;
 			}
-			System.out.println(a.substring(i, i+1));
 		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+
+		String str1 = "aaxxaaxx";
+		String str2 = "iaxxai";
+		int result = stringMatch(str1, str2);
 		System.out.println(result);
 	}
 }
