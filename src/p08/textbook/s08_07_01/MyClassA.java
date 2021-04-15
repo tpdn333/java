8a91792b8b06c67696e2e6049c3ea387be6d0803
tpdn333 <tpdn333@gmail.com>
2021-04-15 15:20:16 +0900

    textbook

---
+++ src/p08/textbook/s08_07_01/MyClassA.java
@@ -0,0 +1,8 @@
+package p08.textbook.s08_07_01;
+
+public class MyClassA implements MyInterface {
+	@Override
+	public void method1() {
+		System.out.println("myclassa - method1");
+	}
+}
---
+++ src/p08/textbook/s08_07_01/MyClassB.java
@@ -0,0 +1,13 @@
+package p08.textbook.s08_07_01;
+
+public class MyClassB implements MyInterface {
+	@Override
+	public void method1() {
+		System.out.println("myclassb-method1()");
+	}
+	
+	@Override
+	public void method2() {
+		System.out.println("myclassb-method2()");
+	}
+}
