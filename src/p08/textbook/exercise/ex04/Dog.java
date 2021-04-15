e342483b66699fb139d5647a11823c5fcb20d688
tpdn333 <tpdn333@gmail.com>
2021-04-15 11:19:24 +0900

    exercise

---
+++ src/p08/textbook/exercise/ex04/Dog.java
@@ -0,0 +1,11 @@
+package p08.textbook.exercise.ex04;
+
+public class Dog implements Soundable {
+
+	@Override
+	public String sound() {
+
+		return "멍멍";
+	}
+
+}
---
+++ src/p08/textbook/exercise/ex05/DataAccessObject.java
@@ -0,0 +1,13 @@
+package p08.textbook.exercise.ex05;
+
+public interface DataAccessObject {
+
+	void select();
+
+	void insert();
+
+	void update();
+
+	void delete();
+
+}
