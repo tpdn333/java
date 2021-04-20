f50baa5cc609df307290c09f03d605c005ad4394
tpdn333 <tpdn333@gmail.com>
2021-04-20 12:47:25 +0900

    exercise

---
+++ src/p10/textbook/exercise/ex07/NotExistIDException.java
@@ -0,0 +1,10 @@
+package p10.textbook.exercise.ex07;
+
+public class NotExistIDException extends Exception{
+	public NotExistIDException() {
+		
+	}
+	public NotExistIDException(String message) {
+		super(message);		
+	}
+}
