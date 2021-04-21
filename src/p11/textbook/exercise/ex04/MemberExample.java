5e82dd4ec18982153fb7dfe361ad572f1e7bf5c4
tpdn333 <tpdn333@gmail.com>
2021-04-21 09:38:00 +0900

    exercise

---
+++ src/p11/textbook/exercise/ex04/MemberExample.java
@@ -0,0 +1,8 @@
+package p11.textbook.exercise.ex04;
+
+public class MemberExample {
+	public static void main(String[] args) {
+		Member m1 = new Member("blue", "이파란");
+		System.out.println(m1);
+	}
+}
