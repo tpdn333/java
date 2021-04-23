ff55a17d0203371adf3fc338bbeffd9a003112ca
tpdn333 <tpdn333@gmail.com>
2021-04-23 14:27:10 +0900

    textbook

--- src/p12/textbook/s120401/Calculator.java
+++ src/p12/textbook/s120401/Calculator.java
@@ -7,14 +7,14 @@ public class Calculator {
 		return memory;
 	}
 
-	public void setMemory(int memory) {
+	public synchronized void setMemory(int memory) {
 		this.memory = memory;
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
-			System.out.println(Thread.currentThread().getName() + ": " + this.memory);
 			e.printStackTrace();
 		}
+		System.out.println(Thread.currentThread().getName() + ": " + this.memory);
 	}
 
 }
