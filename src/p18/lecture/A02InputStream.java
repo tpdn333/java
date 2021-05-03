cc1d98f2547000fbbab245a0876cb89fa0e23033
tpdn333 <tpdn333@gmail.com>
2021-05-03 10:25:20 +0900

    InputStream

---
+++ src/p18/lecture/A02InputStream.java
@@ -0,0 +1,24 @@
+package p18.lecture;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class A02InputStream {
+	public static void main(String[] args) throws Exception {
+		String file = "src/p18/lecture/A02InputStream.java";
+		InputStream is = new FileInputStream(file);
+
+//		int b1 = is.read();
+//		System.out.println(b1);
+
+		int b = 0;
+		int cnt = 0;
+		while ((b = is.read()) != -1) {
+			++cnt;
+			System.out.println(b);
+
+		}
+		System.out.println("총 " + cnt + "byte");
+		is.close();
+	}
+}
