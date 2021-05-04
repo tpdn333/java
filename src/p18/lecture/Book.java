3e3725cbbe975a2d39da520d5a0d058bf3d8e7fe
tpdn333 <tpdn333@gmail.com>
2021-05-04 12:28:53 +0900

    classExample

---
+++ src/p18/lecture/Book.java
@@ -0,0 +1,11 @@
+package p18.lecture;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+	private static final long serialVersionUID = 12345L;
+	
+	private int id;
+	private String name;
+
+}
